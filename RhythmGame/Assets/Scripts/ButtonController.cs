a31c750597169b02b92eeb37f6e64e914af9ef97
Ethne Au <eth.neau515@gmail.com>
2026-03-05 00:04:07 -0500

    Added graybox prototype

--- RhythmGame/Assets/Scripts/ButtonController.cs
+++ RhythmGame/Assets/Scripts/ButtonController.cs
@@ -1,28 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ButtonController : MonoBehaviour
 {
-    private SpriteRenderer spriteRenderer;
+    private MeshRenderer meshRenderer;
+
     public KeyCode keyPressed;
-    public KeyCode keyPressedSecondary; // For the middle key
-    // Start is called before the first frame update
+    public KeyCode keyPressedSecondary = KeyCode.None;
+
+    public float moveSpeed = 3f;
+
+    private bool isStopped = false;
+    private bool isInHitZone = false;
+
     void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material.color = Color.blue;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(keyPressed) || Input.GetKeyDown(keyPressedSecondary))
+        if (!isStopped)
         {
-            spriteRenderer.color = new Color(1, 1, 1, 1f);
+            transform.position += Vector3.down * moveSpeed * Time.deltaTime;
         }
-        if(Input.GetKeyUp(keyPressed) || Input.GetKeyUp(keyPressedSecondary))
+
+        bool keyDown = Input.GetKeyDown(keyPressed) ||
+                       (keyPressedSecondary != KeyCode.None && Input.GetKeyDown(keyPressedSecondary));
+
+        // Only allow hit if inside hit zone
+        if (keyDown && isInHitZone && !isStopped)
         {
-            spriteRenderer.color = new Color(1, 1, 1, 0.10f);
+            HitNote();
+        }
+    }
+
+    void HitNote()
+    {
+        isStopped = true;
+        meshRenderer.material.color = Color.green;
+
+        Destroy(gameObject, 0.3f);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("HitZone"))
+        {
+            isInHitZone = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("HitZone"))
+        {
+            isInHitZone = false;
         }
     }
 }
