fe57a3c25875ed4c40bb36aa7378d211fe25632f
Celia <celia.chan108@gmail.com>
2026-04-06 16:29:02 -0400

    settings script + panel

---
+++ RhythmGame/Assets/Scripts/Settings.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    public GameObject settingsPanel;
+
+    public SongManager songManager;
+
+    public AudioSource audioSource;
+    public AudioSource backgroundNoise;
+    public Slider songVolumeSlider;
+    public Slider bgVolumeSlider;
+    private float prevVolume = 1f;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start()
+    {
+        audioSource = songManager.audioSource;
+
+        songVolumeSlider.value = audioSource.volume;
+        bgVolumeSlider.value = backgroundNoise.volume;
+
+        songVolumeSlider.onValueChanged.AddListener(SetSongVolume);
+        bgVolumeSlider.onValueChanged.AddListener(SetBGVolume);
+        
+
+    }
+    public void ShowPanel()
+    {
+        if(settingsPanel != null)
+        {
+            settingsPanel.SetActive(true);
+        }
+    }
+    public void HidePanel()
+    {
+        if (settingsPanel != null)
+        {
+            settingsPanel.gameObject.SetActive(false);
+        }
+    }
+
+    public void SetSongVolume(float newVolume)
+    {
+        audioSource.volume = newVolume;
+    }
+
+    public void SetBGVolume(float newVolume)
+    {
+        backgroundNoise.volume = newVolume;
+    }
+    public void ToggleMute(AudioSource audio)
+    {
+
+        if(audio.volume > 0f)
+        {
+            prevVolume = audio.volume;
+            audio.volume = 0f;
+        }
+        else
+        {
+            audio.volume = prevVolume;
+        }
+    }
+}
